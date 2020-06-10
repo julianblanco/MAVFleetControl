bb3c992ae98412719e28d360ee5bf828f649aa79
jules <Julian_blnc@yahoo.com>
2020-06-10 17:35:40 -0400

    updated readme

--- muliple_aircraft_fly_to_point.py
+++ muliple_aircraft_fly_to_point.py
@@ -14,18 +14,27 @@ if __name__ == "__main__":
 
 	drone1 = Craft("drone1", "udp://:14540")
 	drone2 = Craft("drone2", "udp://:14541")
-	# loop.run_until_complete(drone.arm(coordinate=[0.0,0.0,0.0],attitude=[0.0,0.0,0.0]))
+
+
 	drone1.start()
 	drone2.start()
+
 	drone1.add_action(FlyToPoint(np.array([0,0,-2]),tolerance = 0.25))
 	drone1.add_action(FlyToPoint(np.array([2,0,-2]),tolerance = 0.25))
+
 	drone2.add_action(FlyToPoint(np.array([0,0,-2]),tolerance = 0.25))
 	drone2.add_action(FlyToPoint(np.array([2,0,-2]),tolerance = 0.25))
+
 	drone1.add_action(land)
 	drone2.add_action(land)
+
+
 	# drone.override_action(land)
+
+
 	drone1.close_conn()#will run after FLYTOPOINT IS DONE)
 	drone2.close_conn()#will run after FLYTOPOINT IS DONE)
+	
 	drone1.join()
 	drone2.join()
 
