8e7c7db7d153646ca4a369c1677a46aba95758d1
jules <Julian_blnc@yahoo.com>
2020-07-04 19:23:50 -0400

    clean up

--- TerminalGCS.py
+++ TerminalGCS.py
@@ -1,4 +1,5 @@
-from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
+from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit.application import run_in_terminal
 from prompt_toolkit.key_binding import KeyBindings
 import os
@@ -13,6 +14,8 @@ from mavfleetcontrol.actions.arm import Arming
 from mavfleetcontrol.actions.kill import Killing
 import asyncio
 import numpy as np
+import zmq
+from zmq.asyncio import Context, Poller
 
 i = 2
 terminalart = r"""
@@ -21,7 +24,7 @@ ___________        _________________   _________
   |    |  ______ /   \  ___/    \  \/ \_____  \ 
   |    | /_____/ \    \_\  \     \____/        \
   |____|erminal   \______  /\______  /_______  /
-						 \/        \/        \/ """
+                         \/        \/        \/ """
 
 print(terminalart)
 bindings = KeyBindings()
@@ -37,30 +40,64 @@ bindings = KeyBindings()
 # 	print(str(x)+'. ' + (valid_scripts[x])[:-3])
 
 loop = True
+remote = True
+url = 'tcp://127.0.0.1:5555'
+url2 = 'tcp://127.0.0.1:5556'
+ctx = Context.instance()
+push = ctx.socket(zmq.PUSH)
+push.bind(url2)
 #-------------------------------------------------------------------
 
+async def receiver():
+	"""receive messages with polling"""
+	pull = ctx.socket(zmq.PULL)
+	pull.connect(url)
+	poller = Poller()
+	poller.register(pull, zmq.POLLIN)
+	while True:
+		events = await poller.poll()
+		if pull in dict(events):
+			# print("recving", events)
+			msg = await pull.recv_multipart()
+			# print('recvd', msg)
 
-	#https://www.geeksforgeeks.org/clear-screen-python/
-def clear(): 
-  
-    # for windows 
-    if os.name == 'nt': 
-        _ = os.system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = os.system('clear') 
+
+async def heartbeat():
+	"""send a message every second"""
+	# push = ctx.socket(zmq.PUSH)
+	# push.bind(url2)
+	
+	msg = '.'
+	while True:
+		# print("sending 0")
+		# await push.send_multipart([msg.encode('ascii')])
+		await push.send_string(msg)
+		await asyncio.sleep(1)
+def send_message(sendmsg):
+	"""send a message every second"""
+	# push = ctx.socket(zmq.PUSH)
+	# push.bind(url2)
+	
+	msg = 'OA'+sendmsg
+	# await push.send_multipart([msg.encode('ascii')])
+	push.send_string(msg)
+
 def print_main_page():
-	clear()
+	os.system('clear')
 	print('1: Connect To Aircraft')
 	print('2: Actions')
 	print('3: Status')
 	print('4: Settings')
+	print()
 
-  
-def connect_to_aircraft():
-	text = prompt('TGCS> How many aircraft?:  ', key_bindings=bindings)
-	text = prompt('TGCS> Serial (1) or IP (2)', key_bindings=bindings)
+async def connect_to_aircraft():
+	session = PromptSession()
+	# text =  await prompt_async('TGCS> How many aircraft?:  ', key_bindings=bindings)
+	with patch_stdout():
+		result = await session.prompt_async('TGCS> How many aircraft?:  ')
+	# text = await prompt_async('TGCS> Serial (1) or IP (2)', key_bindings=bindings)
+	with patch_stdout():
+		result = await session.prompt_async('TGCS> Serial (1) or IP (2)')
 	if text == '1':    
 		if sys.platform == "darwin":
 			serialport = "/dev/tty.usbmodem1"
@@ -86,31 +123,66 @@ def connect_to_aircraft():
 	drone.start()
 	return drone
 
-def actions(drone):
-	clear()
-	print("1: Arm")
-	print("2: Disarm")
-	print("3: Take off")
-	print("4: Precision Land [1,1,0]NED")
-	print("5: Go to [0,0,10] NED")
-	print("6: Circle")
-	print("9: Kill")
-	print("0: Return to main")
-	text = prompt('actions> ', key_bindings=bindings)
-	if text == '1':
-		drone.add_action(Arming())
-	if text == '2':
-		drone.add_action(Disarming())
-	if text == '3':
-		drone.add_action(FlyToPoint(np.array([0,0,-1]),tolerance =1))
-	if text == '4':
-		drone.add_action( PercisionLand( 1.0,   np.array([1, 1])   )  )
-	if text == '5':
-		drone.add_action(FlyToPoint(np.array([0,0,-10]),tolerance =1))
-	if text == '9':
-		drone.override_action(Killing())
-	if text == '0':
-		pass
+async def actions(drone,session):
+	while True:
+		os.system('clear')
+		print("1: Arm")
+		print("2: Disarm")
+		print("3: Take off")
+		print("4: Land")
+		print("5: Precision Land [1,1,0] NED")
+		print("6: Go to [0,0,10] NED")
+		print("7: Circle")
+		print("8: Spin")
+		print("9: Emergency Cut Off")
+		print("0: Back to Main")
+		print()
+		if drone == 'local':
+			local = 0
+		else:
+			local = 1
+		# session = PromptSession()
+		with patch_stdout():
+			text = await session.prompt_async('actions> ')
+		# text = await prompt_async('actions> ', key_bindings=bindings)
+		if text == '1':
+			if local:
+				drone.add_action(Arming())
+			else:
+				send_message('1')
+		if text == '2':
+			if local:
+				drone.add_action(Disarming())
+			else:
+				send_message('2')
+		if text == '3':
+			if local:
+				drone.add_action(FlyToPoint(np.array([0,0,-1]),tolerance =1))
+			else:
+				send_message('3')
+
+		if text == '4':
+			if local:
+				dront.add_action(land)
+			else:
+				send_message('4')
+		if text == '5':
+			if local:
+				drone.add_action( PercisionLand( 1.0,   np.array([1, 1])   )  )
+			else:
+				send_message('5')
+		if text == '6':
+			if local:
+				drone.add_action(FlyToPoint(np.array([0,0,-10]),tolerance =1))
+			else:
+				send_message('6')
+		if text == '9':
+			if local:
+				drone.override_action(Killing())
+			else:
+				send_message('9')
+		if text == '0':
+			break
 
 def status(drone):
 	print('s')
@@ -125,37 +197,51 @@ def settings():
 
 
 
-@bindings.add('c-k')
-def _(event):
-	" Say 'hello' when `c-k` is pressed. "
-	def print_hello():
-		print('hello world')
-	run_in_terminal(print_hello)
+# @bindings.add('c-k')
+# async def _(event):
+# 	" Say 'hello' when `c-k` is pressed. "
+# 	def print_hello():
+# 		print('hello world')
+# 	run_in_terminal(print_hello)
 
-@bindings.add('c-c')
-def _(event):
-	print("\n\n\n\n")
-	print("Good Bye!, may your props be intact")
-	event.app.exit()
+
+
+async def prompt():
+	# @bindings.add('c-c')
+	# async def _(event):
+	# 	print("\n\n\n\n")
+	# 	print("Good Bye!, may your props be intact")
+	# 	event.app.exit()
 	global loop
-	loop = False
+	loop = True
+	print("Welcome!")
+	session = PromptSession()
+
+	drones = 'remote'
+	while loop == True:
+		print_main_page()
+		with patch_stdout():
+			text = await session.prompt_async('TGCS> ')#, key_bindings=bindings)
+		# text = await prompt_async('TGCS> ', key_bindings=bindings)
+		if text == '1':
+			drones = connect_to_aircraft()
+		if text == '2':
+			await actions(drones,session)
+		if text =='3':
+			status(drones)
+		if text == '4':
+			settings()
 
 
+		# drones.override_action('exit')
 #----------------------------------------------------------------------
+if __name__ == "__main__":
+	# Start the main function
+	asyncio.ensure_future(receiver())
+	asyncio.ensure_future(heartbeat())
+	asyncio.ensure_future(prompt())
 
-print("Welcome!")
-drones = None
-while loop == True:
-	print_main_page()
-	text = prompt('TGCS> ', key_bindings=bindings)
-	if text == '1':
-		drones = connect_to_aircraft()
-	if text == '2':
-		actions(drones)
-	if text =='3':
-		status(drones)
-	if text == '4':
-		settings()
-
-
-drones.override_action('exit')
+	# Runs the event loop until the program is canceled with e.g. CTRL-C
+	asyncio.get_event_loop().run_forever()
+	# asyncio.get_event_loop().run_until_complete(asyncio.wait([receiver(), sender(0),]))
+	
--- reciever.py
+++ reciever.py
@@ -8,6 +8,7 @@ from mavfleetcontrol.actions.percision_land import PercisionLand
 from mavfleetcontrol.actions.arm import Arming
 from mavfleetcontrol.actions.disarm import Disarming
 from mavfleetcontrol.actions.land import land
+from mavfleetcontrol.actions.circle import Circle
 
 import zmq
 from zmq.asyncio import Context, Poller
@@ -30,34 +31,39 @@ async def receiver(drone):
     poller = Poller()
     poller.register(pull, zmq.POLLIN)
     while True:
-        events = await poller.poll()
-        if pull in dict(events):
-            # print("recving", events)
-            msg = await pull.recv_multipart()
-            # print((msg[0]))
-            # print(msg.type)
-            if(msg[0]==b'.'):
-                # heartbeat(drone)
-                pass
-            if(((msg[0])[:2])==(b'OA')):
-                # print((msg[0])[2])
-                if(((msg[0])[2])==49):
-                    drone.add_action(Arming())
-                if(((msg[0])[2])==50):
-                    drone.add_action(Disarming())
-                if(((msg[0])[2])==51):
-                    drone.add_action(FlyToPoint(np.array([0, 0, -1]), tolerance=2.5))
-                if(((msg[0])[2])==52):
-                    drone.add_action(land())
-                if(((msg[0])[2])==53):
-                    drone.add_action( PercisionLand( 1.0,   np.array([1, 1])   )  )
-                if(((msg[0])[2])==54):
-                    drone.add_action(FlyToPoint(np.array([0,0,-10]),tolerance =1))
-                if(((msg[0])[2])==55):
-                    drone.add_action(Circle(velocity=20.0,radius=8.0,angle=0.0))
-                if(((msg[0])[2])==59):
-                    drone.override_action(Killing())
 
+            try:
+            events = await poller.poll()
+            if pull in dict(events):
+                # print("recving", events)
+                msg = await pull.recv_multipart()
+                # print((msg[0]))
+                # print(msg.type)
+                if(msg[0]==b'.'):
+                    # heartbeat(drone)
+                    pass
+                if(((msg[0])[:2])==(b'OA')):
+                    # print((msg[0])[2])
+                    if(((msg[0])[2])==49):
+                        drone.add_action(Arming())
+                    if(((msg[0])[2])==50):
+                        drone.add_action(Disarming())
+                    if(((msg[0])[2])==51):
+                        drone.add_action(FlyToPoint(np.array([0, 0, -1]), tolerance=2.5))
+                    if(((msg[0])[2])==52):
+                        drone.add_action(land())
+                    if(((msg[0])[2])==53):
+                        drone.add_action( PercisionLand( 1.0,   np.array([1, 1])   )  )
+                    if(((msg[0])[2])==54):
+                        drone.add_action(FlyToPoint(np.array([0,0,-10]),tolerance =1))
+                    if(((msg[0])[2])==55):
+                        drone.add_action(Circle(velocity=20.0,radius=8.0,angle=0.0))
+                    if(((msg[0])[2])==57):
+                        drone.override_action(Killing())
+                    if(((msg[0])[2])==57):
+                        drone.override_action(Killing())
+            except Exception as E:
+                print(E)
 
 
 
